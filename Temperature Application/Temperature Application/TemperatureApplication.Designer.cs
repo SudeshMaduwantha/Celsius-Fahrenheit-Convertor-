6082ac508096bf6b3a733aec5ee4c41254d70031
Sudesh Maduwanthadaka <81201477+SudeshMaduwantha@users.noreply.github.com>
2023-11-02 15:44:54 +0530

    Add files via upload
    This is my own created simple software to convert Fahrenheit to Celsius and Celsius to Fahrenheit .
    I used c# language to create this software.
    @Sudesh Maduwantha

---
+++ Temperature Application/Temperature Application/TemperatureApplication.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Temperature_Application
+{
+    partial class TemperatureApplication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1_GetInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1_GetInput);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 528);
+            this.panel1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Celsius",
+            "Fahrenheit"});
+            this.comboBox1.Location = new System.Drawing.Point(454, 197);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(244, 39);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkViolet;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(285, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 67);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1_GetInput
+            // 
+            this.textBox1_GetInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1_GetInput.Location = new System.Drawing.Point(454, 288);
+            this.textBox1_GetInput.Name = "textBox1_GetInput";
+            this.textBox1_GetInput.Size = new System.Drawing.Size(244, 38);
+            this.textBox1_GetInput.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Input Temperature";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(366, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Input Temperature Format";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Cyan;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(15, 15);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(770, 114);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(595, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temperature Application";
+            // 
+            // TemperatureApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 528);
+            this.Controls.Add(this.panel1);
+            this.Name = "TemperatureApplication";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Temperature Application";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1_GetInput;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Temperature Application/Temperature Application/TemperatureApplication.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Temperature_Application
+{
+    public partial class TemperatureApplication : Form
+    {
+        public TemperatureApplication()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /*private object comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }*/
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                if(float.TryParse(textBox1_GetInput.Text,out float celsius_value))
+                {
+                    float Calculated_Fahrenheit_Value = (celsius_value * 9 / 5) + 32;
+
+                    MessageBox.Show($"Fahrenheit Value:{Calculated_Fahrenheit_Value}");
+
+                }
+                else
+                {
+                    MessageBox.Show($"Invalid Input. Please Enter Valid Number!!!");
+                }
+            }
+            else if(comboBox1.SelectedIndex == 1) 
+            {
+                if(float.TryParse(textBox1_GetInput.Text,out float fahrenheight_value))
+                {
+                    float Calculated_Celsius_Value = (fahrenheight_value - 32) * 5 / 9;
+
+                    MessageBox.Show($"Celsius Value:{Calculated_Celsius_Value}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Input . Please Enter A Valid Number!!!");
+            }
+        }
+    }
+}
+ 
